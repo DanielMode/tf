8c33190454b5145b456f107f32e0b583a969850d
Daniel Mode <modedaniel4@gmail.com>
2023-03-11 11:52:53 +0100

    used golden image ami, added ssh and outbound rule

--- main.tf
+++ main.tf
@@ -3,10 +3,37 @@ provider "aws" {
   profile = "aws_sso"
 }
 resource "aws_instance" "example" {
-ami = "ami-0557a15b87f6559cf"
-instance_type = "t2.micro"
+  ami = "ami-006f0be65e536ded6"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  
 
-tags = {
-  Name = "terraform-example"
+  tags = {
+    Name = "terraform-example"
 }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
